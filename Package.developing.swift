fdbebf72084e8f59cbdf3612afc0965c7331433d
Luke Woydziak <luke.woydziak@gmail.com>
2022-10-20 01:21:27 -0600

    [#183587817] Allow ios SDK to support 11

--- Package.developing.swift
+++ Package.developing.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
         name: "xenon_view_sdk",
         platforms: [
-            .macOS(.v12), .iOS(.v15), .tvOS(.v15)
+            .macOS(.v12), .iOS(.v11), .tvOS(.v15)
         ],
         products: [
             // Products define the executables and libraries a package produces, and make them visible to other packages.
